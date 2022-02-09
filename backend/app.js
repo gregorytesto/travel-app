255cd3111b9dbde1e659e9be103916cfe622122b
Greg Testo <gregt@pursuit.org>
2022-02-09 12:18:54 -0400

    added database and get all places endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
 const cors = require("cors");
+
+const placesController = require("./controllers/placesController.js");
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res)=>{
     res.send("<h1>Welcome to our travel app</h1>");
 })
+
+app.use("/places", placesController);
 
 module.exports = app;
